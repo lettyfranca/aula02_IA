3a25e6b0ce854a8be317ad934db1332c55fdedd6
lettyfranca <leeffrance@gmail.com>
2024-03-12 00:24:38 -0300

    is working

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-let dist, km, tempo,carro,climaRuim,chuva;
+let dist = prompt("Digite a distância em KM:")
+let tempo = prompt("Digite o tempo percorrido:")
+let cinema = prompt("O cinema fica na cidade? (true/false)");
+let climaRuim = prompt("O clima está ruim? (true/false)");
 
 if (dist > 5){
     carro = true;
-} 
-
-if (dist > 1 && tempo < 15){
+    console.log(carro)
+} else if (dist > 1 && tempo < 15){
     carro = true;
-}
-
-if (dist > 1 && tempo > 15){
+    console.log(carro)
+} else if (dist > 1 && tempo > 15){
     carro = false;
+    console.log(carro)
 }
 
-if (carro == true && cinema == true){
-    carro = false;
-    taxi = true;
-}
-
-if (carro == true && cinema == false){
-    carro = true;
-}
-
-if (carro == false && climaRuim == true){
-    chuva = true;
-}
-
-if (carro == false && climaRuim == false){
-    console.log("ritmo de passeio")
+if (carro == true){
+    if (cinema == true){
+        carro = false;
+        taxi = true;
+        console.log(carro, taxi)
+    } else {
+        carro = true;
+        console.log(carro)
+    }
+} else {
+    if (climaRuim == true) {
+        chuva = true;
+        console.log(chuva)
+    } else {
+        console.log("ritmo de passeio")
+    }
 }
